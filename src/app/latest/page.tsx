d31cd11cf9852eaf7f8730938e35de77bd014ab2
TechYoichiro <y.119.wonder@gmail.com>
2025-05-07 00:48:55 +0900

    fix folder function

--- src/app/latest/page.tsx
+++ src/app/latest/page.tsx
@@ -4,6 +4,7 @@ import { Tabs } from "@/app/components/layouts/Tabs"
 import { fetchLatestArticles } from '../lib/api/article';
 import { ArticleListSkeleton } from "@/app/components/features/articles/ArticleListSkeleton"
 import { Suspense } from 'react';
+import { isErrorResponse } from '@/app/types/article';
 
 export default async function Page() {
   const items = await fetchLatestArticles();
@@ -18,12 +19,12 @@ export default async function Page() {
       <Tabs items={tabItems} className="mb-4" />
       <div className="w-full max-w-4xl">
         <Suspense fallback={<ArticleListSkeleton />}>
-          {Array.isArray(items) ? (
-            <ArticleList items={items} />
-          ) : (
+          {isErrorResponse(items) ? (
             <div className="text-center py-10">
               <p className="text-red-500">{items.error}</p>
             </div>
+          ) : (
+            <ArticleList items={items.articles} />
           )}
         </Suspense>
       </div>
